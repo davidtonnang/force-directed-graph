c828ab0cabda3b6e88832998b9f378b608619ec0
Davidko Barilla <davidtonnang@outlook.com>
2023-07-03 10:48:57 +0200

    created a label disposition with button

--- D3-project/examples/experiment/index.js
+++ D3-project/examples/experiment/index.js
@@ -6,12 +6,16 @@ fetch("../datasets/co_data_test.json")
       ...new Set(data.nodes.map((node) => node.therapy_areas)),
     ]
 
+    // Defines a function that is globally accessible for the label buttons
+    window.handleButtonClick = function () {
+      d3.select("svg").selectAll(".clickedLabelGroup").remove()
+    }
+
     const filterDropdown = d3.select("#filterDropdown")
 
     const privateCheckbox = d3.select("#privateCheckbox")
     const publicCheckbox = d3.select("#publicCheckbox")
     const employeeRange = document.getElementById("employeeRange")
-
 
     privateCheckbox.on("change", handleFilterSelection)
     publicCheckbox.on("change", handleFilterSelection)
@@ -27,97 +31,103 @@ fetch("../datasets/co_data_test.json")
     filterDropdown.on("change", handleFilterDropdown)
 
     // Initialize filterState
-let filterState = {
-  therapyArea: "",
-  financing: {
-    private: false,
-    listed: false,
-  },
-  minEmployees: 0,
-};
-
-// Function to apply filters
-function applyFilters() {
-  // Use the filterState to filter nodes
-  nodes
-    .attr("r", (d) => {
-      // Adjust radius based on filters
-      let radius = d.size;
-      if (filterState.therapyArea !== "" && d.therapy_areas.includes(filterState.therapyArea)) {
-        radius *= 1.0;
-      }
-      if ((!filterState.financing.private && !filterState.financing.public) 
-          || (filterState.financing.private && d.financing === "Private") 
-          || (filterState.financing.public && d.financing === "Listed")) {
-        radius *= 1.0;
-      }
-      if (d.amount_of_employees >= filterState.minEmployees) {
-        radius *= 1.0;
-      }
-      return radius;
-    })
-    .style("opacity", (d) => {
-      // Adjust opacity based on filters
-      let opacity = 1;
-      if (filterState.therapyArea !== "" && !d.therapy_areas.includes(filterState.therapyArea)) {
-        opacity = 0.2;
-      }
-      if ((filterState.financing.private || filterState.financing.public) 
-          && !(filterState.financing.private && d.financing === "Private") 
-          && !(filterState.financing.public && d.financing === "Listed")) {
-        opacity = 0.2;
-      }
-      if (!(d.amount_of_employees >= filterState.minEmployees)) {
-        opacity = 0.2;
-      }
-      return opacity;
-    });
-}
-
-
-// Handle therapy area filter
-function handleFilterDropdown() {
-  filterState.therapyArea = this.value;
-  applyFilters();
-}
-
-// Handle financing filter
-function handleFilterSelection() {
-  filterState.financing.private = privateCheckbox.property("checked");
-  filterState.financing.public = publicCheckbox.property("checked");
-  applyFilters();
-
-  privateCheckbox.on("change", function() {
-    if (this.checked) {
-      publicCheckbox.property("checked", false);
+    let filterState = {
+      therapyArea: "",
+      financing: {
+        private: false,
+        listed: false,
+      },
+      minEmployees: 0,
     }
-    handleFilterSelection.call(this);
-  })
-  
-  publicCheckbox.on("change", function() {
-    if (this.checked) {
-      privateCheckbox.property("checked", false);
+
+    // Function to apply filters
+    function applyFilters() {
+      // Use the filterState to filter nodes
+      nodes
+        .attr("r", (d) => {
+          // Adjust radius based on filters
+          let radius = d.size
+          if (
+            filterState.therapyArea !== "" &&
+            d.therapy_areas.includes(filterState.therapyArea)
+          ) {
+            radius *= 1.0
+          }
+          if (
+            (!filterState.financing.private && !filterState.financing.public) ||
+            (filterState.financing.private && d.financing === "Private") ||
+            (filterState.financing.public && d.financing === "Listed")
+          ) {
+            radius *= 1.0
+          }
+          if (d.amount_of_employees >= filterState.minEmployees) {
+            radius *= 1.0
+          }
+          return radius
+        })
+        .style("opacity", (d) => {
+          // Adjust opacity based on filters
+          let opacity = 1
+          if (
+            filterState.therapyArea !== "" &&
+            !d.therapy_areas.includes(filterState.therapyArea)
+          ) {
+            opacity = 0.2
+          }
+          if (
+            (filterState.financing.private || filterState.financing.public) &&
+            !(filterState.financing.private && d.financing === "Private") &&
+            !(filterState.financing.public && d.financing === "Listed")
+          ) {
+            opacity = 0.2
+          }
+          if (!(d.amount_of_employees >= filterState.minEmployees)) {
+            opacity = 0.2
+          }
+          return opacity
+        })
     }
-    handleFilterSelection.call(this);
-  })
-}
 
-// Handle employee range filter
-function handleEmployeeRangeSelection() {
-  filterState.minEmployees = Math.pow(2, employeeRange.value);
-  // update displayed value
-  d3.select("#employeeValue").text(filterState.minEmployees);
-  applyFilters();
-}
+    // Handle therapy area filter
+    function handleFilterDropdown() {
+      filterState.therapyArea = this.value
+      applyFilters()
+    }
 
-// Attach event handlers to filters
-filterDropdown.on("change", handleFilterDropdown);
-privateCheckbox.on("change", handleFilterSelection);
-publicCheckbox.on("change", handleFilterSelection);
-employeeRange.addEventListener("input", handleEmployeeRangeSelection);
+    // Handle financing filter
+    function handleFilterSelection() {
+      filterState.financing.private = privateCheckbox.property("checked")
+      filterState.financing.public = publicCheckbox.property("checked")
+      applyFilters()
 
+      privateCheckbox.on("change", function () {
+        if (this.checked) {
+          publicCheckbox.property("checked", false)
+        }
+        handleFilterSelection.call(this)
+      })
 
-    
+      publicCheckbox.on("change", function () {
+        if (this.checked) {
+          privateCheckbox.property("checked", false)
+        }
+        handleFilterSelection.call(this)
+      })
+    }
+
+    // Handle employee range filter
+    function handleEmployeeRangeSelection() {
+      filterState.minEmployees = Math.pow(2, employeeRange.value)
+      // update displayed value
+      d3.select("#employeeValue").text(filterState.minEmployees)
+      applyFilters()
+    }
+
+    // Attach event handlers to filters
+    filterDropdown.on("change", handleFilterDropdown)
+    privateCheckbox.on("change", handleFilterSelection)
+    publicCheckbox.on("change", handleFilterSelection)
+    employeeRange.addEventListener("input", handleEmployeeRangeSelection)
 
     // Create a function that links two nodes
     const DEFAULT_DISTANCE = 30
@@ -212,78 +222,51 @@ employeeRange.addEventListener("input", handleEmployeeRangeSelection);
       .style("fill", "white")
       .style("visibility", "hidden")
 
-    // Update the label text on mouseover
+    // Shows labels on mouseover
     nodes
       .on("mouseover", function (event, d) {
-        // Calculate the scaled coordinates relative to the current zoom level and the node's position
         const transform = d3.zoomTransform(svg.node())
         const scaledX = d.x * transform.k + transform.x
         const scaledY = d.y * transform.k + transform.y
 
-        
+        // Create a group to hold the foreignObject and label
+        const labelGroup = svg
+          .append("g")
+          .attr("class", "labelGroup")
+          .style("visibility", "hidden")
 
-        // Creates a new label
-    const label = svg
-    .append("text")
-    .data([d])
-    .attr("class", "label")
-    .html((labelData) =>
-      `<tspan>${labelData.id}</tspan>` +
-      (labelData.amount_of_employees ? `<tspan x="0" dy="1.2em">Employees: ${labelData.amount_of_employees}</tspan>` : '') +
-      `<tspan x="0" dy="1.2em">Therapy Area: ${labelData.therapy_areas}</tspan>` +
-      (labelData.financing ? `<tspan x="0" dy="1.2em">Financing: ${labelData.financing}</tspan>` : '')
-    )
-    .style("visibility", "visible")
-    .style("fill", "white")
-    .attr("x", scaledX)
-    .attr("y", scaledY - 10) // Adjust the y position to position the label above the node
+        // Select the existing foreignObject in the group
+        const foreignObject = labelGroup
+          .append("foreignObject")
+          .attr("x", scaledX + 15) // adjust position
+          .attr("y", scaledY - 50) // adjust position
+          .attr("width", 300) // set width
+          .attr("height", 400) // set height
+          .html(
+            `<div class="info-box info-box-hidden">
+        ${d.company_logo ? `<img src="${d.company_logo}" />` : ""}
+           <h4>${d.id}</h4>
+           <p>${
+             d.amount_of_employees ? `Employees: ${d.amount_of_employees}` : ""
+           }</p>
+           <p>${d.therapy_areas ? `Therapy Area: ${d.therapy_areas}` : ""}</p>
+           <p>${d.financing ? `Financing: ${d.financing}` : ""}</p>
+           <button onclick="handleButtonClick()">Click Me</button>
+         </div>`
+          )
 
-        label
-          .selectAll("tspan")
-          .attr("x", scaledX + 15)
-          .attr("dy", "1.2em")
+        setTimeout(() => {
+          document
+            .querySelector(".info-box")
+            .classList.remove("info-box-hidden")
+        }, 10)
+
+        labelGroup.style("visibility", "visible") // make the labelGroup visible
       })
 
-
-      nodes
-  .on("mouseover", function (event, d) {
-    const transform = d3.zoomTransform(svg.node())
-    const scaledX = d.x * transform.k + transform.x
-    const scaledY = d.y * transform.k + transform.y
-
-
-    // Create a group to hold the foreignObject and label
-    const labelGroup = svg.append("g").attr("class", "labelGroup")
-
-    // Append a foreignObject to the group
-    const foreignObject = labelGroup
-      .append("foreignObject")
-      .attr("x", scaledX + 15) // adjust position
-      .attr("y", scaledY - 50) // adjust position
-      .attr("width", 300) // set width
-      .attr("height", 400) // set height
-      .html(
-        `<div class="info-box info-box-hidden">
-        ${d.company_logo ? `<img src="${d.company_logo}" />` : ''}
-           <h4>${d.id}</h4>
-           <p>${d.amount_of_employees ? `Employees: ${d.amount_of_employees}` : ''}</p>
-           <p>${d.therapy_areas ? `Therapy Area: ${d.therapy_areas}` : ''}</p>
-           <p>${d.financing ? `Financing: ${d.financing}` : ''}</p>
-         </div>`
-      )
-
-      setTimeout(() => {
-        document.querySelector('.info-box').classList.remove('info-box-hidden');
-      }, 10);
-      
-  })
-
-  .on("mouseout", function (event, d) {
-    svg.selectAll(".labelGroup").remove() // remove group on mouseout
-  })
-
-  
-
+      .on("mouseout", function (event, d) {
+        svg.selectAll(".labelGroup").remove() // remove group on mouseout
+      })
 
       // Adds a smooth zoom function on click for the nodes
       .on("click", function (event, d) {
@@ -304,6 +287,43 @@ employeeRange.addEventListener("input", handleEmployeeRangeSelection);
             d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale)
           )
       })
+
+    // Shows labels on click
+    nodes.on("click", function (event, d) {
+      const transform = d3.zoomTransform(svg.node())
+      const scaledX = d.x * transform.k + transform.x
+      const scaledY = d.y * transform.k + transform.y
+
+      console.log("click")
+
+      // Removes any already existing labels
+      svg.selectAll(".clickedLabelGroup").remove()
+
+      // Create a new label group for the clicked node
+      const clickedLabelGroup = svg
+        .append("g")
+        .attr("class", "clickedLabelGroup")
+
+      // Append a foreignObject to the group
+      const foreignObject = clickedLabelGroup
+        .append("foreignObject")
+        .attr("x", scaledX + 15) // adjust position
+        .attr("y", scaledY - 50) // adjust position
+        .attr("width", 300) // set width
+        .attr("height", 400) // set height
+        .html(
+          `<div class="info-click-box info-click-box-hidden">
+        ${d.company_logo ? `<img src="${d.company_logo}" />` : ""}
+           <h4>${d.id}</h4>
+           <p>${
+             d.amount_of_employees ? `Employees: ${d.amount_of_employees}` : ""
+           }</p>
+           <p>${d.therapy_areas ? `Therapy Area: ${d.therapy_areas}` : ""}</p>
+           <p>${d.financing ? `Financing: ${d.financing}` : ""}</p>
+           <button onclick="handleButtonClick()">Click Me</button>
+         </div>`
+        )
+    })
 
     // Updates the node and link positions on each tick of the simulation
     simulation.on("tick", () => {
