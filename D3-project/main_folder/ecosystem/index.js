703775e0dbd08a4e71be2a4eb6f10cc2d113e035
Gustafsson <kjlv039@astrazeneca.net>
2023-07-07 13:38:39 +0200

    hej

--- D3-project/main_folder/ecosystem/index.js
+++ D3-project/main_folder/ecosystem/index.js
@@ -23,12 +23,12 @@ fetch("https://davidtonnang.github.io/force-directed-graph/co_data_test.json")
     //}
 
     // Function that looks for string in a word, and removes it and everything after if it finds it
-    function remove_all_after(word, char) {
-      if (word.includes(char)) {
-        let index = word.indexOf(char)
-        filtered_string = word.slice(0, index)
+    function remove_all_after(sentence, char) {
+      if (sentence.includes(char)) {
+        let index = sentence.indexOf(char)
+        filtered_string = sentence.slice(0, index)
       } else {
-        filtered_string = word
+        filtered_string = sentence
       }
 
       return filtered_string
@@ -282,7 +282,7 @@ fetch("https://davidtonnang.github.io/force-directed-graph/co_data_test.json")
 
     // Create a group for the graph elements
 
-    const container = svg.append("g")
+    const container = svg.append("g") //Is g = group?
 
     // Enable zooming and panning behavior
     const zoom = d3.zoom().on("zoom", (event) => {
@@ -310,7 +310,7 @@ fetch("https://davidtonnang.github.io/force-directed-graph/co_data_test.json")
       )
 
     // In defs we're going to add the images in the nodes
-    var defs = svg.append("defs")
+    var defs = container.append("defs")
 
     // Create the nodes
     const nodes = container
@@ -318,7 +318,7 @@ fetch("https://davidtonnang.github.io/force-directed-graph/co_data_test.json")
       .data(data.nodes)
       .enter()
       .append("circle")
-      .style("fill", (d) => "url(#" + d.id + ")")
+      .style("fill", (d) => "url(#" + d.id + ")") // puts the image in the node
       .attr("class", "node")
       .style("cursor", "pointer")
       .attr("r", (node) => node.size)
@@ -416,6 +416,7 @@ fetch("https://davidtonnang.github.io/force-directed-graph/co_data_test.json")
     var bvh_x = data.nodes[0].x // Important that bvh is first in json
     var bvh_y = data.nodes[0].y
 
+    // Here we adjust position of the labels based on position of nodes in image
     for (let i = 0; i < data.nodes.length; i++) {
       if (
         data.nodes[i].y > bvh_y &&
