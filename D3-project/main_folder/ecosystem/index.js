f5b2a0fb1865f7aeadfd07253543996aeaf7674a
David Tönnäng <108948879+davidtonnang@users.noreply.github.com>
2023-07-10 06:57:48 +0000

    Merge fix to update index.js

--- D3-project/main_folder/ecosystem/index.js
+++ D3-project/main_folder/ecosystem/index.js
@@ -10,7 +10,6 @@ fetch("../datasets/co_data_test.json")
       ...new Set(data.nodes.map((node) => node.type_of_company)),
     ]
 
-    // Sets which nodes are visible
     data.nodes.forEach((node) => {
       node.isVisible = ["BioVentureHub", "Astra", "GoCo"].includes(node.id)
     })
@@ -28,12 +27,12 @@ fetch("../datasets/co_data_test.json")
     //}
 
     // Function that looks for string in a word, and removes it and everything after if it finds it
-    function remove_all_after(sentence, char) {
-      if (sentence.includes(char)) {
-        let index = sentence.indexOf(char)
-        filtered_string = sentence.slice(0, index)
+    function remove_all_after(word, char) {
+      if (word.includes(char)) {
+        let index = word.indexOf(char)
+        filtered_string = word.slice(0, index)
       } else {
-        filtered_string = sentence
+        filtered_string = word
       }
 
       return filtered_string
@@ -265,14 +264,12 @@ fetch("../datasets/co_data_test.json")
     connectNodes("GoCo", "BioVentureHub", 300)
     connectNodes("Astra", "BioVentureHub", 300)
     connectNodes("GoCo", "Astra", 300)
-    connectNodes("Astra", "BVH_Companies", 600)
-    connectNodes("GoCo", "BVH_Companies", 600)
     // Create the SVG container
     const svg = d3.select("#graph")
 
     // Create a group for the graph elements
 
-    const container = svg.append("g") //Is g = group?
+    const container = svg.append("g")
 
     // Enable zooming and panning behavior
     const zoom = d3.zoom().on("zoom", (event) => {
@@ -314,7 +311,7 @@ fetch("../datasets/co_data_test.json")
       )
 
     // In defs we're going to add the images in the nodes
-    var defs = container.append("defs")
+    var defs = svg.append("defs")
 
     // Create the nodes
     const nodes = container
@@ -322,7 +319,7 @@ fetch("../datasets/co_data_test.json")
       .data(data.nodes)
       .enter()
       .append("circle")
-      .style("fill", (d) => "url(#" + d.id + ")") // puts the image in the node
+      .style("fill", (d) => "url(#" + d.id + ")")
       .attr("class", "node")
       .style("cursor", "pointer")
       .attr("r", (node) => node.size)
@@ -424,7 +421,6 @@ fetch("../datasets/co_data_test.json")
     var bvh_x = data.nodes[0].x // Important that bvh is first in json
     var bvh_y = data.nodes[0].y
 
-    // Here we adjust position of the labels based on position of nodes in image
     for (let i = 0; i < data.nodes.length; i++) {
       if (
         data.nodes[i].y > bvh_y &&
